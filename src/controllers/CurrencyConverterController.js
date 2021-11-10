9ad42fc7847d3fdbb4857c80c6a0f17cf0a3a150
William Rodrigues <williamrodrigues2442@gmail.com>
2021-11-10 16:53:44 -0300

    Changes logic to converts currency and treats errors

--- src/controllers/CurrencyConverterController.js
+++ src/controllers/CurrencyConverterController.js
@@ -6,13 +6,14 @@ function CurrencyConverterController(app) {
    *   http://localhost:8000/exchange/10/BRL/USD/4.50
    */
   app.get('/exchange/:amount/:from/:to/:rate', (req, res) => {
-    const amount = parseFloat(req.params.amount),
-          rate = parseFloat(req.params.rate),
-          from = req.params.from.toUpperCase(),
-          to = req.params.to.toUpperCase();
+    const params = {
+      amount: parseFloat(req.params.amount),
+      rate: parseFloat(req.params.rate),
+      from: req.params.from.toUpperCase(),
+      to: req.params.to.toUpperCase(),
+    };
     
-    if (from === 'BRL' && to === 'USD')
-      CurrencyConverter.BRLtoUSD(amount, rate, res);
+    CurrencyConverter.convert(params, res);
   });
 }
 
--- src/models/CurrencyConverter.js
+++ src/models/CurrencyConverter.js
@@ -1,16 +1,50 @@
 class CurrencyConverter {
-  BRLtoUSD(amount, rate, res) {
-    if (!isNaN(amount) && !isNaN(rate)) {
-      res.status(200).json({
-        valorConvertido: rate * amount,
-        simboloMoeda: '$',
-      });
+  _simbols = { BRL: 'R$', USD: '$', EUR: '€' };
+  _conversions = ['BRL', 'USD', 'EUR'];
+
+  convert(params, res) {
+    const errors = this._validateParams(params);
+
+    if (errors.length) {
+      res.status(400).json(errors);
     } else {
-      res.status(400).json({
-        erro: true,
-        msg: 'Os valores para conversão precisam ser números válidos',
+      res.status(200).json({
+        valorConvertido: params.rate * params.amount,
+        simboloMoeda: this._simbols[params.to],
       });
     }
+  }
+
+  /**
+   * to and from params should be one of: 'BRL', 'USD' and 'EUR'.
+   * amount and rate params should be >= 0 and be numbers.
+   */
+  _validateParams(params) {
+    const validations = [
+      {
+        paramName: 'from',
+        isValid: this._conversions.includes(params.from),
+        errMsg: `Valor passado inválido, precisa ser um dos ${this._conversions.join(', ')}`, 
+      },
+      {
+        paramName: 'to',
+        isValid: this._conversions.includes(params.to),
+        errMsg: `Valor passado inválido, precisa ser um dos ${this._conversions.join(', ')}`, 
+      },
+      {
+        paramName: 'amount',
+        isValid: !isNaN(params.amount) && params.amount >= 0,
+        errMsg: `Valor passado inválido, precisa ser um número maior que 0`, 
+      },
+      {
+        paramName: 'rate',
+        isValid: !isNaN(params.rate) && params.rate >= 0,
+        errMsg: `Valor passado inválido, precisa ser um número maior que 0`, 
+      },
+    ];
+
+    // Get errors, if any.
+    return validations.filter((validationObj) => !validationObj.isValid);
   }
 }
 
